9884cbca1c666c5296d48631738d02046ed121d6
sirrj4rvis <aryan96936@gmail.com>
2026-05-12 11:34:41 +0530

    first bad code

---
+++ badcode.py
@@ -0,0 +1,59 @@
+# BAD PYTHON CODE WITH MULTIPLE ERRORS
+
+import maths
+
+def calculate_average(numbers)
+    total = 0
+
+    for i in range(len(numbers)):
+        total = total + numbers[i]
+
+    avg = total / len(number)
+    return avg
+
+
+class Student
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", age)
+
+
+numbers = [10, 20, 30, 40]
+
+average = calculate_average(numbers)
+
+print("Average is: " + average)
+
+student1 = Student("Aryan", 20)
+
+student1.show()
+
+
+for i in range(5)
+    print(i)
+
+x = int("hello")
+
+if x > 5
+print("Greater")
+else:
+    print("Smaller")
+
+data = {"name": "John", "age": 22}
+
+print(data["address"])
+
+list = [1, 2, 3]
+print(list[10])
+
+a = 10
+b = 0
+
+print(a / b)
+
+while True
+    print("Infinite Loop")
